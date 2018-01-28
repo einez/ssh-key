ba121f598c43c2e2fe805ed911f9044f383529bf
einez <1.0eine.z@gmail.com>
2018-01-28 22:52:14 +0800

    gen with jsch

--- src/GenKey.java
+++ src/GenKey.java
@@ -1,2 +1,39 @@
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.KeyPair;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 public class GenKey {
+    public static void main(String[] args){
+        Map<String,String> keys = new HashMap<>();
+        int type = KeyPair.RSA;
+        JSch jsch = new JSch();
+        try {
+            KeyPair kpair = KeyPair.genKeyPair(jsch, type,2048);
+            //私钥
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();//向OutPutStream中写入
+            kpair.writePrivateKey(baos);
+            String privateKeyString = baos.toString();
+            //公钥
+            baos = new ByteArrayOutputStream();
+            kpair.writePublicKey(baos,"comment");
+            String publicKeyString = baos.toString();
+            System.out.println("Finger print: " + kpair.getFingerPrint());
+            kpair.dispose();
+            // 得到公钥字符串
+//          String publicKeyString = RSAEncrypt.loadPublicKeyByFile(filePath,filename + ".pub");
+//          System.out.println(publicKeyString.length());
+            System.out.println(publicKeyString);
+            keys.put("publicKey", publicKeyString);
+            // 得到私钥字符串
+//          String privateKeyString = RSAEncrypt.loadPrivateKeyByFile(filePath,filename);
+//          System.out.println(privateKeyString.length());
+            System.out.println(privateKeyString);
+            keys.put("privateKey", privateKeyString);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
